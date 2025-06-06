031ec22bdc7658815cf7a948e91631763d87b671
Anders Åslund <anders.aslund@teknoir.ai>
2025-06-06 09:26:44 +0200

    Refactor SCP scripts to improve variable handling and add recursive option

--- scripts/scp_from_device.sh
+++ scripts/scp_from_device.sh
@@ -56,11 +56,11 @@ else
   export DEVICE_MANIFEST="$(kubectl --context ${CONTEXT} -n $NAMESPACE get $DEVICE_RESOURCE $DEVICE -o yaml)"
 fi
 
-USERNAME="$(echo "$DEVICE_MANIFEST" | yq e .spec.keys.data.username - | base64 --decode -i -)"
-PASSWORD="$(echo "$DEVICE_MANIFEST" | yq e .spec.keys.data.userpassword - | base64 --decode -i -)"
+USERNAME="$(printf "%s\n" "${DEVICE_MANIFEST}" | yq e .spec.keys.data.username - | base64 --decode -i -)"
+PASSWORD="$(printf "%s\n" "${DEVICE_MANIFEST}" | yq e .spec.keys.data.userpassword - | base64 --decode -i -)"
 
-REMOTE_ACCESS_ACTIVE="$(echo "$DEVICE_MANIFEST" | yq e .subresources.status.remote_access.active -)"
-REMOTE_ACCESS_PORT="$(echo "$DEVICE_MANIFEST" | yq e .subresources.status.remote_access.port -)"
+REMOTE_ACCESS_ACTIVE="$(printf "%s\n" "${DEVICE_MANIFEST}" | yq e .subresources.status.remote_access.active -)"
+REMOTE_ACCESS_PORT="$(printf "%s\n" "${DEVICE_MANIFEST}" | yq e .subresources.status.remote_access.port -)"
 
 echo "NAMESPACE             = ${NAMESPACE}"
 echo "DEVICE                = ${DEVICE}"
@@ -75,7 +75,7 @@ if ! [[ ${REMOTE_ACCESS_ACTIVE} == true ]] ; then
 fi
 
 RSA_KEY_FILE=$(mktemp -t "$(basename $0)")
-echo "$DEVICE_MANIFEST" | yq e .spec.keys.data.rsa_private - | base64 --decode -i - | tee ${RSA_KEY_FILE} >/dev/null
+printf "%s\n" "${DEVICE_MANIFEST}" | yq e .spec.keys.data.rsa_private - | base64 --decode -i - | tee ${RSA_KEY_FILE} >/dev/null
 
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
@@ -85,6 +85,6 @@ DEADENDUSER='teknoir'
 DEADENDHOST="deadend.${DOMAIN}"
 DEADENDPORT='2222'
 PROXY_CMD="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ExitOnForwardFailure=yes -o ServerAliveInterval=60 -i ${RSA_KEY_FILE} -N -W %h:%p ${DEADENDUSER}@${DEADENDHOST} -p ${DEADENDPORT}"
-scp -o "ProxyCommand ${PROXY_CMD}" -o 'UserKnownHostsFile=/dev/null' -o 'StrictHostKeyChecking=no' -o 'ServerAliveInterval=60' -i ${RSA_KEY_FILE} -P ${REMOTE_ACCESS_PORT} ${USERNAME}@127.0.0.1:${SOURCE} ${TARGET}
+scp -r -o "ProxyCommand ${PROXY_CMD}" -o 'UserKnownHostsFile=/dev/null' -o 'StrictHostKeyChecking=no' -o 'ServerAliveInterval=60' -i ${RSA_KEY_FILE} -P ${REMOTE_ACCESS_PORT} ${USERNAME}@127.0.0.1:${SOURCE} ${TARGET}
 
 rm ${RSA_KEY_FILE}
--- scripts/scp_to_device.sh
+++ scripts/scp_to_device.sh
@@ -55,11 +55,11 @@ else
   export DEVICE_MANIFEST="$(kubectl --context ${CONTEXT} -n $NAMESPACE get $DEVICE_RESOURCE $DEVICE -o yaml)"
 fi
 
-USERNAME="$(echo "$DEVICE_MANIFEST" | yq e .spec.keys.data.username - | base64 --decode -i -)"
-PASSWORD="$(echo "$DEVICE_MANIFEST" | yq e .spec.keys.data.userpassword - | base64 --decode -i -)"
+USERNAME="$(printf "%s\n" "${DEVICE_MANIFEST}" | yq e .spec.keys.data.username - | base64 --decode -i -)"
+PASSWORD="$(printf "%s\n" "${DEVICE_MANIFEST}" | yq e .spec.keys.data.userpassword - | base64 --decode -i -)"
 
-REMOTE_ACCESS_ACTIVE="$(echo "$DEVICE_MANIFEST" | yq e .subresources.status.remote_access.active -)"
-REMOTE_ACCESS_PORT="$(echo "$DEVICE_MANIFEST" | yq e .subresources.status.remote_access.port -)"
+REMOTE_ACCESS_ACTIVE="$(printf "%s\n" "${DEVICE_MANIFEST}" | yq e .subresources.status.remote_access.active -)"
+REMOTE_ACCESS_PORT="$(printf "%s\n" "${DEVICE_MANIFEST}" | yq e .subresources.status.remote_access.port -)"
 
 echo "NAMESPACE             = ${NAMESPACE}"
 echo "DEVICE                = ${DEVICE}"
@@ -74,7 +74,7 @@ if ! [[ ${REMOTE_ACCESS_ACTIVE} == true ]] ; then
 fi
 
 RSA_KEY_FILE=$(mktemp -t "$(basename $0)")
-echo "$DEVICE_MANIFEST" | yq e .spec.keys.data.rsa_private - | base64 --decode -i - | tee ${RSA_KEY_FILE} >/dev/null
+printf "%s\n" "${DEVICE_MANIFEST}" | yq e .spec.keys.data.rsa_private - | base64 --decode -i - | tee ${RSA_KEY_FILE} >/dev/null
 
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
@@ -84,6 +84,6 @@ DEADENDUSER='teknoir'
 DEADENDHOST="deadend.${DOMAIN}"
 DEADENDPORT='2222'
 PROXY_CMD="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ExitOnForwardFailure=yes -o ServerAliveInterval=60 -i ${RSA_KEY_FILE} -N -W %h:%p ${DEADENDUSER}@${DEADENDHOST} -p ${DEADENDPORT}"
-scp -o "ProxyCommand ${PROXY_CMD}" -o 'UserKnownHostsFile=/dev/null' -o 'StrictHostKeyChecking=no' -o 'ServerAliveInterval=60' -i ${RSA_KEY_FILE} -P ${REMOTE_ACCESS_PORT} ${SOURCE} ${USERNAME}@127.0.0.1:${TARGET}
+scp -r -o "ProxyCommand ${PROXY_CMD}" -o 'UserKnownHostsFile=/dev/null' -o 'StrictHostKeyChecking=no' -o 'ServerAliveInterval=60' -i ${RSA_KEY_FILE} -P ${REMOTE_ACCESS_PORT} ${SOURCE} ${USERNAME}@127.0.0.1:${TARGET}
 
 rm ${RSA_KEY_FILE}
